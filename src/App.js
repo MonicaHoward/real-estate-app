76b7a4e9bfdc43f0a41a8e1fa8ccb7732bf769af
Monica <jay.m.howard@gmail.com>
2023-04-05 21:28:56 -0500

    app mvp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import AllProperties from './components/AllProperties';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Property from './components/Property';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link className="router-link" to="/">Home</Link>{' '}
+        <Link className="router-link" to="/shop">Shop</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />}>
+          <Route path="" element={<AllProperties />} />
+          <Route path=":id" element={<Property />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/PropertiesList.js
@@ -0,0 +1,21 @@
+const PropertiesList = {
+
+    "1": {
+        name: "1234 Main St",
+        image: "https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    "2": {
+        name: "125534 Peach Tree St",
+        image: "https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    "3": {
+        name: "34th Street",
+        image: "https://images.pexels.com/photos/2183521/pexels-photo-2183521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    "4": {
+        name: "234 Mango St",
+        image: "https://images.pexels.com/photos/2587054/pexels-photo-2587054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    }
+}
+
+export default PropertiesList;
